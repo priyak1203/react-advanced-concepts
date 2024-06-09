31edb8e4fe97483ecbb55cafe546d7ef5f626040
Priydarshini Karanth <kpriya1203@gmail.com>
2024-06-09 16:52:57 +0530

    error example before state

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ErrorExample from './tutorial/01-useState/01-error-example';
+
+function App() {
+  return (
+    <div className="container">
+      {/* <h2>react advanced concepts</h2> */}
+      <ErrorExample />
+    </div>
+  );
+}
+
+export default App;
