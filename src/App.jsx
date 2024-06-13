894794f664e522c951ef69a1cf5cf24cbdea3e6d
Priydarshini Karanth <kpriya1203@gmail.com>
2024-06-13 10:47:32 +0530

    need for useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import UseStateBasics from './tutorial/01-useState/02-useState-basics';
 import UseStateArray from './tutorial/01-useState/03-useState-array';
 import UseStateObject from './tutorial/01-useState/04-useState-object';
 import UseStateGotcha from './tutorial/01-useState/05-useState-gotcha';
+import CodeExample from './tutorial/02-useEffect/01-code-example';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       {/* <UseStateBasics /> */}
       {/* <UseStateArray /> */}
       {/* <UseStateObject /> */}
-      <UseStateGotcha />
+      {/* <UseStateGotcha /> */}
+      <CodeExample />
     </div>
   );
 }
