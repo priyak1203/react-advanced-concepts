9b3ea75c3b88e99e2397f08b406adade608a4d41
Priydarshini Karanth <kpriya1203@gmail.com>
2024-06-14 20:48:35 +0530

    multiple returns basics

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import CodeExample from './tutorial/02-useEffect/01-code-example';
 import UseEffectBasics from './tutorial/02-useEffect/02-useEffect-basics';
 import MultipleEffects from './tutorial/02-useEffect/03-multiple-effects';
 import FetchData from './tutorial/02-useEffect/04-fetch-data';
+import MultipleReturnsBasics from './tutorial/03-conditional-rendering/01-multiple-returns-basics';
 
 function App() {
   return (
@@ -20,7 +21,8 @@ function App() {
       {/* <CodeExample /> */}
       {/* <UseEffectBasics /> */}
       {/* <MultipleEffects /> */}
-      <FetchData />
+      {/* <FetchData /> */}
+      <MultipleReturnsBasics />
     </div>
   );
 }
