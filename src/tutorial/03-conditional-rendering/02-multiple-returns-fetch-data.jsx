950514020458db7febd31d289da77aa9f18e4ece
Priydarshini Karanth <kpriya1203@gmail.com>
2024-06-15 16:19:52 +0530

    refactoring fetch data

--- src/tutorial/03-conditional-rendering/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/02-multiple-returns-fetch-data.jsx
@@ -7,27 +7,27 @@ const MultipleReturnsFetchData = () => {
   const [isError, setIsError] = useState(false);
   const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const respsonse = await fetch(url);
+  const fetchUser = async () => {
+    try {
+      const respsonse = await fetch(url);
 
-        // fix for fetch error "gotcha"
-        if (!respsonse.ok) {
-          setIsError(true);
-          setIsLoading(false);
-          return;
-        }
-
-        const user = await respsonse.json();
-        setUser(user);
-      } catch (error) {
+      // fix for fetch error "gotcha"
+      if (!respsonse.ok) {
         setIsError(true);
-        console.log(error);
+        setIsLoading(false);
+        return;
       }
-      setIsLoading(false);
-    };
 
+      const user = await respsonse.json();
+      setUser(user);
+    } catch (error) {
+      setIsError(true);
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
     fetchUser();
   }, []);
 
@@ -39,16 +39,18 @@ const MultipleReturnsFetchData = () => {
     return <h2>There was an error</h2>;
   }
 
+  const { avatar_url, name, company, bio } = user;
+
   return (
     <div>
       <img
-        src={user.avatar_url}
-        alt={user.name}
+        src={avatar_url}
+        alt={name}
         style={{ width: '150px', borderRadius: '25px' }}
       />
-      <h2>{user.name}</h2>
-      <h4>works at {user.company}</h4>
-      <p>{user.bio}</p>
+      <h2>{name}</h2>
+      <h4>works at {company}</h4>
+      <p>{bio}</p>
     </div>
   );
 };
