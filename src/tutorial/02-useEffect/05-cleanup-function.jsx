88e7a9c0c28bd0d54b659450fee1d1799446c482
Priydarshini Karanth <kpriya1203@gmail.com>
2024-06-17 20:29:25 +0530

    cleanup function timer example

--- src/tutorial/02-useEffect/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/05-cleanup-function.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 const CleanupFunction = () => {
   const [toggle, setToggle] = useState(false);
 
+  console.log('render');
+
   return (
     <div>
       <button className="btn" onClick={() => setToggle(!toggle)}>
@@ -16,6 +18,14 @@ const CleanupFunction = () => {
 const Greeting = () => {
   useEffect(() => {
     console.log('Hello from Greetings - useEffect');
+    const intId = setInterval(() => {
+      console.log('Hello from Interval');
+    }, 1000);
+
+    return () => {
+      clearInterval(intId);
+      console.log('cleanup');
+    };
   }, []);
 
   return <h3 style={{ marginTop: '1rem' }}>Hello There</h3>;
