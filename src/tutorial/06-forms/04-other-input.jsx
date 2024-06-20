1c845e26c19e819ad59ecc809c7fd13aee72468b
Priydarshini Karanth <kpriya1203@gmail.com>
2024-06-20 12:23:46 +0530

    select input handling

--- src/tutorial/06-forms/04-other-input.jsx
+++ src/tutorial/06-forms/04-other-input.jsx
@@ -4,9 +4,14 @@ const frameworks = ['react', 'angular', 'vue', 'svelte'];
 
 const OtherInputs = () => {
   const [shipping, setShipping] = useState(false);
+  const [framework, setFramework] = useState('');
 
   const handleShipping = (e) => {
     setShipping(e.target.checked);
+  };
+
+  const handleFramework = (e) => {
+    setFramework(e.target.value);
   };
 
   return (
@@ -33,10 +38,26 @@ const OtherInputs = () => {
             onChange={handleShipping}
           />
         </div>
+        {/* select */}
         <div className="form-row" style={{ textAlign: 'left' }}>
-          <label htmlFor="framework" className="form-label">
+          <label
+            htmlFor="framework"
+            className="form-label"
+            style={{ display: 'inline', marginRight: '1rem' }}
+          >
             framework
           </label>
+          <select
+            name="framework"
+            id="framework"
+            value={framework}
+            onChange={handleFramework}
+            style={{ textTransform: 'capitalize' }}
+          >
+            {frameworks.map((framework, index) => {
+              return <option key={index}>{framework}</option>;
+            })}
+          </select>
         </div>
         <button type="submit" className="btn btn-block">
           submit
