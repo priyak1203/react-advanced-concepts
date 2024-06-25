e58a266adbc5f80ff45a0033029bdf92e7d22050
Priydarshini Karanth <kpriya1203@gmail.com>
2024-06-25 23:42:47 +0530

    actions and reducer

---
+++ src/tutorial/10-useReducer/reducer.js
@@ -0,0 +1,24 @@
+import { data } from '../../data';
+import { CLEAR_LIST, REMOVE_ITEM, RESET_LIST } from './actions';
+
+const reducer = (state, action) => {
+  if (action.type === CLEAR_LIST) {
+    return { ...state, people: [] };
+  }
+
+  if (action.type === RESET_LIST) {
+    return { ...state, people: data };
+  }
+
+  if (action.type === REMOVE_ITEM) {
+    const newPeople = state.people.filter(
+      (person) => person.id !== action.payload.id
+    );
+    return { ...state, people: newPeople };
+  }
+
+  //   return state;
+  throw new Error(`No matching "${action.type}" - action type`);
+};
+
+export default reducer;
