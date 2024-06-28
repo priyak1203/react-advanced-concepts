97c0984ca27084c7bd5807f052cc1fcd54af6162
Priydarshini Karanth <kpriya1203@gmail.com>
2024-06-28 11:55:17 +0530

    useTransition complete

--- src/tutorial/11-performance/06-useTransition/index.jsx
+++ src/tutorial/11-performance/06-useTransition/index.jsx
@@ -1,21 +1,25 @@
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 
 const ReactUseTransition = () => {
   const [text, setText] = useState('');
   const [items, setItems] = useState([]);
 
+  const [isPending, startTransition] = useTransition();
+
   const handleChange = (e) => {
     setText(e.target.value);
 
     // slowdown CPU
-    const newItems = Array.from({ length: 5000 }, (_, index) => {
-      return (
-        <div key={index}>
-          <img src="/vite.svg" alt="" />
-        </div>
-      );
+    startTransition(() => {
+      const newItems = Array.from({ length: 5000 }, (_, index) => {
+        return (
+          <div key={index}>
+            <img src="/vite.svg" alt="" />
+          </div>
+        );
+      });
+      setItems(newItems);
     });
-    setItems(newItems);
   };
 
   return (
@@ -30,15 +34,19 @@ const ReactUseTransition = () => {
       </form>
 
       <h4>items below</h4>
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '1fr 1fr 1fr',
-          marginTop: '2rem',
-        }}
-      >
-        {items}
-      </div>
+      {isPending ? (
+        <h5>Loading...</h5>
+      ) : (
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr 1fr',
+            marginTop: '2rem',
+          }}
+        >
+          {items}
+        </div>
+      )}
     </section>
   );
 };
